c848884fa114053644440fdb2579337c58ca26af
gabor <gabor@localhost.localdomain>
2023-09-06 23:05:59 +0200

    cleanup

--- init.el
+++ init.el
@@ -114,9 +114,26 @@
 	("C-." . embark-dwim)
 	("M-/" . embark-export))
 (use-package embark-consult)
+<<<<<<< HEAD
 (use-package eat
 	:demand t
 	:hook (eshell-load . eat-eshell-mode))
+=======
+(use-package helpful
+  :demand t
+  :bind
+  (("C-h f" . helpful-callable)
+   ("C-h v" . helpful-variable)
+   ("C-h k" . helpful-key)
+   ("C-h F" . helpful-function)
+   ("C-h C" . helpful-command)
+   ("C-c C-d" . helpful-at-point)))
+(use-package eat
+	:demand t
+	:hook (eshell-load . eat-eshell-mode))
+(use-package elfeed
+  :demand t)
+>>>>>>> b643470 (cleanup)
 (use-package xclip
   :demand t
   :config (xclip-mode))
@@ -197,6 +214,13 @@
 	:straight nil
 	:requires (orderless cape)
 	:demand t
+<<<<<<< HEAD
+=======
+	:hook
+	(c-ts-mode . eglot-ensure)
+	(zig-mode . eglot-ensure)
+	(c++-ts-mode . eglot-ensure)
+>>>>>>> b643470 (cleanup)
 	:config (advice-add 'eglot-completion-at-point :around #'cape-wrap-buster) (setq completion-category-overrides '((eglot (styles orderless))))
 	(add-hook 'eglot-managed-mode-hook #'(lambda () (setq-local completion-at-point-functions
 																															(list (cape-super-capf #'eglot-completion-at-point #'cape-file)))))
@@ -206,11 +230,15 @@
 							("C-c C-e C-r" . eglot-rename)
 							("C-c C-e r" . eglot-rename)
 							("C-c C-e C-f" . eglot-format-buffer)
+<<<<<<< HEAD
 							("C-c C-e f" . eglot-format-buffer))
 	:hook
 	(c-ts-mode . eglot-ensure)
 	(zig-mode . eglot-ensure)
 	(c++-ts-mode . eglot-ensure))
+=======
+							("C-c C-e f" . eglot-format-buffer)))
+>>>>>>> b643470 (cleanup)
 
 ;; magit
 (use-package magit
@@ -326,6 +354,11 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+<<<<<<< HEAD
+=======
+ '(elfeed-feeds
+   '("https://www.youtube.com/feeds/videos.xml?channel_id=UCdJdEguB1F1CiYe7OEi3SBg" "https://www.youtube.com/feeds/videos.xml?channel_id=UCtowLlQSH6QRtp0-Z26U17A" "https://www.youtube.com/feeds/videos.xml?channel_id=UCKtix2xNNXdcEfEFnoOnvMw" "https://www.youtube.com/feeds/videos.xml?channel_id=UCRC6cNamj9tYAO6h_RXd5xA" "https://www.youtube.com/feeds/videos.xml?channel_id=UCAiiOTio8Yu69c3XnR7nQBQ" "https://www.youtube.com/feeds/videos.xml?channel_id=UCBa659QWEk1AI4Tg--mrJ2A" "https://www.youtube.com/feeds/videos.xml?channel_id=UCQD3awTLw9i8Xzh85FKsuJA" "https://www.youtube.com/feeds/videos.xml?channel_id=UClyGlKOhDUooPJFy4v_mqPg" "https://www.youtube.com/feeds/videos.xml?channel_id=UCnQC_XGCCI__qrxwgZS27-A" "https://www.youtube.com/feeds/videos.xml?channel_id=UCcnci5vbpLJ-rh_V9yXwawg" "https://www.youtube.com/feeds/videos.xml?channel_id=UCS0N5baNlQWJCUrhCEo8WlA" "https://www.youtube.com/feeds/videos.xml?channel_id=UCD6VugMZKRhSyzWEWA9W2fg" "https://www.youtube.com/feeds/videos.xml?channel_id=UCtHaxi4GTYDpJgMSGy7AeSw" "https://www.youtube.com/feeds/videos.xml?channel_id=UCCnILYoBNuR4qaUOynGWzRg" "https://www.youtube.com/feeds/videos.xml?channel_id=UCJ0-OtVpF0wOKEqT2Z1HEtA" "https://www.youtube.com/feeds/videos.xml?channel_id=UCFLwN7vRu8M057qJF8TsBaA"))
+>>>>>>> b643470 (cleanup)
  '(inhibit-startup-screen t)
  '(warning-suppress-types '((comp) (emacs) comp)))
 (custom-set-faces
